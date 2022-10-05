f0972f4f8a858fa81b4741816342bcd8c6c564fc
danfilof <danila.anfilofyev@gmail.com>
2022-10-05 23:56:03 +0200

    Watched until 55:31

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,3 @@
-    create table products (id bigserial primary key, title varchar(255), cost int);
-    insert into products (title, cost) values
+    create table products (id bigserial primary key, title varchar(255), price int);
+    insert into products (title, price) values
     ('Milk', 80), ('Bread', 25), ('Cheese', 300);
